ba336d11870f4e183147970e620f3449467ca4fe
Majida86 <malloukmajida2@gmail.com>
2023-04-01 14:36:53 +0200

    commit

--- javascript/form.js
+++ javascript/form.js
@@ -1,17 +1,26 @@
 //On va chercher les differents éléments de notre page
 const pages = document.querySelectorAll(".page")
+const header = document.querySelector("header")
 const nbPages = pages.length //nb de pages à récupere
+let pageActive = 1
 
 //on attend le chargement de la page
 window.onload = () => {
     //on affiche la première page du formulaire
 document.querySelector(".page").style.display = "initial"
 
+
 // On gère les boutons "suivant"
-let boutons = document.querySelectorAll("button[type=button]")
+let boutons = document.querySelectorAll(".next")
 
 for(let bouton of boutons){
-    boutons.addEventListener("click", pageSuivante)
+    bouton.addEventListener("click", pageSuivante)
+}
+// On gère les boutons "precedent"
+ boutons = document.querySelectorAll(".prev")
+
+for(let bouton of boutons){
+    bouton.addEventListener("click", pagePrecedente)
 }
 
 }
@@ -30,3 +39,20 @@ for(let page of pages){
 // On affiche la page suivante 
 this.parentElement.nextElementSibling.style.display = "initial"
 }
+
+// cette fonction fait reculer le formulaire d'une page
+
+function pagePrecedente(){
+    // On incrémente pageActive
+pageActive--
+
+// On masque toutes les pages
+for(let page of pages){
+    page.style.display = "none"
+}
+
+// On affiche la page precedente
+this.parentElement.previousElementSibling.style.display = "initial"
+}
+
+
